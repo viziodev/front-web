649d681f2c7210a059cc03547fae88de74f15374
viziodev <viziodev03@gmail.com>
2023-06-15 09:06:31 +0000

    Lister Les utilisateurs

--- src/app/securite/users/users.component.ts
+++ src/app/securite/users/users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MyResponse } from 'src/app/shared/models/response';
+import { User } from 'src/app/shared/models/user';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  users:User[]=[];
+  constructor(private userService:UserService) { }
 
   ngOnInit(): void {
+     this.userService.all().subscribe((res:MyResponse)=>{
+          this.users=res.data
+     })
   }
 
 }
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MyResponse } from '../models/response';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  all(): Observable<MyResponse> {
+    return this.http.get<MyResponse>(`${environment.api}/users`);
+  }
 }
